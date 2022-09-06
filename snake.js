c0a64cb638103d62c672d4efe2d4e14362d95155
Jorgv <1043229@mborijnland.nl>
2022-09-06 14:08:14 +0200

    delete rocks

--- snake.js
+++ snake.js
@@ -1,1 +1,6 @@
-alert("LO1E-SWD4 rocks!");
+document.getElementById('snake');
+const canvas = document.getElementById("snake");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
