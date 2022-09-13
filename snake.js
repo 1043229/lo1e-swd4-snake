151e3ee203c10e642765f33ad0586c2b17414c22
Jorgv <1043229@mborijnland.nl>
2022-09-13 13:22:13 +0200

    Set interval geplaats en function

--- snake.js
+++ snake.js
@@ -2,9 +2,15 @@ document.getElementById('snake');
 const canvas = document.getElementById("snake");
 const ctx = canvas.getContext("2d");
 
+// draw the background
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, 400, 400);
 
+// draw the snake
 ctx.fillStyle = "green";
-ctx.fillRect(190, 180, 20, 20);
+ctx.fillRect(200, 200, 20, 20);
 
+function update() {
+    console.log('update');
+}
+setInterval(update, 1000);
