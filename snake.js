74022d4083a96d24fee865584dca7cd72915250e
Jorgv <1043229@mborijnland.nl>
2022-10-04 13:16:17 +0200

    snake kan niet 180graden terug

--- snake.js
+++ snake.js
@@ -99,13 +99,23 @@ function update() {
 
 function changeDirection(event) {
     if(event.code == 'ArrowUp') {
-        direction = 'up';
+        if (direction !='down'){
+            direction = 'up';
+        }
     } else if (event.code == 'ArrowDown') {
-        direction = 'down';
+        if(direction !='up'){
+           direction = 'down';
+        }
+        
     } else if (event.code == 'ArrowLeft') {
-        direction = 'left';
+        if(direction !='right'){
+           direction = 'left';  
+        }
+    
     } else if (event.code == 'ArrowRight') {
-        direction = 'right';
+        if(direction != left){
+           direction = 'right';
+        }
     }
 }
 
